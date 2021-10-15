4025dbd55848f0fecdd62653f486ca5dd1aaefb3
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-10-16 00:39:20 +0400

    test css

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {SuperInputText} from "../../common/SuperComponents/c1-SuperInputText/SuperInputText";
 import {SuperCheckbox} from "../../common/SuperComponents/c3-SuperCheckbox/SuperCheckbox";
 import {SuperButton} from "../../common/SuperComponents/c2-SuperButton/SuperButton";
+import s from './Login.module.css'
 
 export const Login = () => {
     return (
-        <div>
-            <div>Login</div>
+        <div className={s.inner}>
+            
             <SuperInputText/>
             <SuperInputText/>
             <SuperCheckbox/> <span>Remember me</span>
