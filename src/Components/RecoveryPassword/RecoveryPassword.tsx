4a06f87e7902ae65c85ef687d972e727fd84bbe4
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-10-19 09:42:26 +0400

    working with password recovery styles

--- src/Components/RecoveryPassword/RecoveryPassword.tsx
+++ src/Components/RecoveryPassword/RecoveryPassword.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SuperInputText} from "../../common/SuperComponents/c1-SuperInputText/SuperInputText";
 import {SuperButton} from "../../common/SuperComponents/c2-SuperButton/SuperButton";
+import s from "./RecoveryPassword.module.css"
+import logo from '../../images/logo/logo.png';
 
 type RecoveryPasswordPropsType = {
     sendClick: () => void
@@ -11,13 +13,18 @@ type RecoveryPasswordPropsType = {
 
 export const RecoveryPassword = (props: RecoveryPasswordPropsType) => {
     return (
-        <div>
-            <h2>Forgot your password?</h2>
-            <div>
-                <span>Login - </span>
-                <SuperInputText onChangeText={props.onChangeEmail} value={props.email}/>
-                <SuperButton name={'send'} onClick={props.sendClick}/>
-            </div>
+        <div className={s.inner}>
+            <div className={s.wrap}>
+                    <img className={s.img} src={logo}  alt="" />
+                </div>
+            <h2 className={s.title}>Forgot your password?</h2>
+                <form className={s.form} action="">
+                    <SuperInputText onChangeText={props.onChangeEmail} placeholder={'Email'} value={props.email}/>
+                    <label style={{marginTop:'15px'}} htmlFor="Email">Enter your email address and we will send you further instructions</label>
+                    <SuperButton style={{padding: '9px 70px', marginTop:'100px'}} name={'Send Instructions'} onClick={props.sendClick}/>
+                </form>
+                <span className={s.span}>Did you remember your password?</span>
+
         </div>
     );
 };
