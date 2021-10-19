002ac3c51fafba62e846e3d85a82db22b0d6d083
AleksandrBoguckiy <vagula91@gmail.com>
2021-10-19 17:06:03 +0700

    commiy regist

--- src/Components/Auth/AuthContainer.tsx
+++ src/Components/Auth/AuthContainer.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Auth} from "./Auth";
+import {useDispatch} from "react-redux";
+import {setSuccessRegAC} from "../../Store/registrationReducer";
 
 export const AuthContainer = () => {
-
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(setSuccessRegAC(false))
+    }, [])
     return (
         <Auth/>
     )
