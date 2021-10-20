e164e8bb6aa6870318639623b199e8becf569357
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-10-20 21:40:48 +0400

    add component new password and style

--- src/Components/NewPassword/NewPassword.tsx
+++ src/Components/NewPassword/NewPassword.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import {SuperInputText} from "../../common/SuperComponents/c1-SuperInputText/SuperInputText";
 import {SuperButton} from "../../common/SuperComponents/c2-SuperButton/SuperButton";
+import s from './NewPassword.module.css'
+import logo from '../../images/logo/logo.png'
 
 type PropsType = {
     onChangeNewPass: (password: string) => void,
@@ -13,13 +15,18 @@ type PropsType = {
 export const NewPassword = (props: PropsType) => {
     if(props.testMessage==='success') return <Redirect to={'/'}/>
     return (
-        <div>
+        <div className={s.inner}>
+             <div className={s.wrap}>
+                <img className={s.img} src={logo} alt="" />
+            </div>
+            <h1 className={s.title}>Create new password</h1> 
             <div>{props.testMessage}</div>
-            Create new password
-            <SuperInputText type='password' placeholder='new password' onChangeText={props.onChangeNewPass}
-                            value={props.newPassword}/>
-            <SuperButton name={'send'} onClick={props.sendNewPass}/>
+            <div className={s.form}>
+                <SuperInputText type='password' placeholder='New password' onChangeText={props.onChangeNewPass} value={props.newPassword}/>
+                <p className={s.text}>Create new password and we will send you further instructions to email</p>
         </div>
+                <SuperButton style={{marginTop:'90px'}} name={'Create new password'} onClick={props.sendNewPass}/>
+            </div>
     );
 };
 
