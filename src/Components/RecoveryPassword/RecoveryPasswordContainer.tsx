9429f56c2e4d78b85df02b893e9c17a28443ea0d
Alex <tarasovas1988@mail.ru>
2021-10-20 15:06:04 +0300

    last version

--- src/Components/RecoveryPassword/RecoveryPasswordContainer.tsx
+++ src/Components/RecoveryPassword/RecoveryPasswordContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../Store/redux-store";
 import {InitialStateType, loginVerificationTC, setLoginAC} from "../../Store/forgotPasswordReducer";
